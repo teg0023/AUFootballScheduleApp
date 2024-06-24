87057ba2c6379a16866d2f321fa2d202942b47c5
teg0023 <teg0023@auburn.edu>
2024-06-24 09:02:26 -0500

    Initial Commit

---
+++ AUFootballSchedule/AUFootballScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AUFootballScheduleApp.swift
+//  AUFootballSchedule
+//
+//  Created by user254330 on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct AUFootballScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
