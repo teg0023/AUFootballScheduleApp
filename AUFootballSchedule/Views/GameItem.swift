3a5b9752acabb20b3a0f0d48489a1c0da83e3fad
teg0023 <teg0023@auburn.edu>
2024-06-25 06:50:48 -0500

    Created a view. Setting up its format.

---
+++ AUFootballSchedule/Views/GameItem.swift
@@ -0,0 +1,18 @@
+//
+//  GameItem.swift
+//  AUFootballSchedule
+//
+//  Created by user254330 on 6/25/24.
+//
+
+import SwiftUI
+
+struct GameItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameItem()
+}
